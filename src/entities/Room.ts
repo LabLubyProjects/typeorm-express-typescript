092b7cc8bcce81f13016245318fd5c517d66f053
Breno Vergilio <bjvergilio47@gmail.com>
2022-09-05 14:16:42 -0400

    feat: add subject entity

--- src/entities/Room.ts
+++ src/entities/Room.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToMany, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Subject } from "./Subject";
 import { Video } from "./Video";
 
 @Entity('rooms')
@@ -11,4 +12,7 @@ export class Room {
 
   @OneToMany(() => Video, video => video.room)
   videos: Video[]
+
+  @ManyToMany(() => Subject, subject => subject.rooms)
+  subjects: Subject[]
 }
