e2e3df26e2fd1ac22d5819a3a0ff05eade9a6da0
Breno Vergilio <bjvergilio47@gmail.com>
2022-09-05 12:41:16 -0400

    feat: set app up

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,7 +4,7 @@ import { DataSource } from "typeorm";
 
 const port = process.env.DB_PORT as number | undefined;
 
-const AppDataSource = new DataSource({
+export const AppDataSource = new DataSource({
   type: 'mysql',
   host: process.env.DB_HOST,
   port,
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { AppDataSource } from "./data-source";
+
+AppDataSource.initialize().then(() => {
+  const app = express();
+  app.use(express.json());
+  return app.listen(process.env.PORT, () => console.log('Server is up'));
+})
